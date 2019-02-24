3b43afaaad9ee7a399b51df5368403d11f7034ef
Michael Bordogna <mike@mvsoftinc.com>
2019-02-24 11:15:04 -0500

    add auto versioning

--- Solution Items/GlobalAssembly.cs
+++ Solution Items/GlobalAssembly.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 
-[assembly: AssemblyDescription("Trident")]
-[assembly: AssemblyCompany("Get Trident")]
+[assembly: AssemblyDescription("Trident is a .NET Standard, Core, and Framework Reference Archutecure that is prodution ready for SOLID App Dev.")]
+[assembly: AssemblyCompany("Modern Visionary Software")]
 [assembly: AssemblyProduct("Trident")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Trident")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
