2798fb12d5f68fd140c13e5a5157fe4d39d7c10c
Michael Bordogna <mike@mvsoftinc.com>
2019-03-01 14:34:16 -0500

    remove the autofac package, everything is in the main trident packages atm

--- Standard/Trident.Autofac/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Trident.Autofac")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3D0A0904-DD2D-479C-94C5-F837ECF29C80")]
-
